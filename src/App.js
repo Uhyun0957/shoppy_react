03e259bea436bc13b35cb2f65c34198cebd386b4
Uhyun0957 <cyhaz0957@gmail.com>
2021-01-19 22:14:12 +0900

    create Navbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
-import Products from "./components/Products/Products";
+import React, { useState, useEffect } from "react";
+import { shoppy } from "./lib/shoppy";
+
+import { Products, Navbar } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await shoppy.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   );
---
+++ src/lib/shoppy.js
@@ -0,0 +1,3 @@
+import Shoppy from "@chec/commerce.js";
+
+export const shoppy = new Shoppy(process.env.REACT_APP_CHECK_PUBLIC_KEY, true);
