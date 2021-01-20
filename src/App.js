d0a6c2816b69bc793d55bb2308bde3768e98dbb3
Uhyun0957 <cyhaz0957@gmail.com>
2021-01-20 22:18:24 +0900

    add to cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Products, Navbar } from "./components";
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await shoppy.products.list();
@@ -12,14 +13,27 @@ const App = () => {
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await shoppy.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quanitity) => {
+    const item = await shoppy.cart.add(productId, quanitity);
+
+    setCart(item.cart);
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
+
+  console.log(cart);
 
   return (
     <div>
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 };
